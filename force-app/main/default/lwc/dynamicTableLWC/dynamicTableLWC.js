692ec9db51020252a182d9980ae2902db7e4d711
RanjithGitHub <ranjith71223@gmail.com>
2023-07-30 18:09:08 +0530

    lwc table commit

--- force-app/main/default/lwc/dynamicTableLWC/dynamicTableLWC.js
+++ force-app/main/default/lwc/dynamicTableLWC/dynamicTableLWC.js
@@ -1,108 +1,64 @@
-import {LightningElement, track, api} from "lwc";
+import { LightningElement, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+export default class dynamicTableLWC extends NavigationMixin(LightningElement) {
 
-export default class dynamicTableLWC extends LightningElement {
-	@track _columns = [];
-	@track _data = [];
-	@track _inputColumns;
+keyIndex = 0;
+@track itemList = [
+    {
+        id: 0
+    }
+];
 
-	@api
-	get columns() {
-		return this._columns;
-	}
+addRow() {
+    ++this.keyIndex;
+    var newItem = [{ id: this.keyIndex }];
+    this.itemList = this.itemList.concat(newItem);
+}
 
-	set columns(value) {
-		this._columns = value;
-		if (value) {
-			let tempCols = [...value];
-			tempCols.push({type: "button-icon", typeAttributes: {iconName: "utility:delete", name: "delete", iconClass: "slds-icon-text-error"}, fixedWidth: 50});
-			tempCols.push({type: "button-icon", typeAttributes: {iconName: "utility:edit", name: "edit"}, fixedWidth: 50});
-			this._columns = tempCols;
+removeRow(event) {
+    if (this.itemList.length >= 2) {
+        this.itemList = this.itemList.filter(function (element) {
+			console.log(event.target.accessKey);
+			console.log(element.id);
+            return parseInt(element.id) !== parseInt(event.target.accessKey);
+        });
+    }
+}
 
-			let colData = JSON.parse(JSON.stringify(value));
-			colData = colData.filter((column) => column.fieldName);
-			colData.forEach((element) => {
-				if (this.tempRow.hasOwnProperty(element.fieldName)) {
-					element.displayValue = this.tempRow[element.fieldName];
-				}
-			});
-			this._inputColumns = colData;
-		}
-	}
+handleSubmit() {
+    var isVal = true;
+    this.template.querySelectorAll('lightning-input-field').forEach(element => {
+        isVal = isVal && element.reportValidity();
+    });
+    if (isVal) {
+        this.template.querySelectorAll('lightning-record-edit-form').forEach(element => {
+            element.submit();
+        });
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message: 'Contacts successfully created',
+                variant: 'success',
+            }),
+        );
+        // Navigate to the Account home page
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Contact',
+                actionName: 'home',
+            },
+        });
+    } else {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Error creating record',
+                message: 'Please enter all the required fields',
+                variant: 'error',
+            }),
+        );
+    }
+}
 
-	
-// Added via GitHub  Added via VSCode
-	@track showModal;
-	@track tempRow = {};
-	editMode = false;
-
-	handleRowAction(event) {
-		if (event.detail.action.name === "delete") {
-			this.deleteSelectedRow(event.detail.row);
-		} else if (event.detail.action.name === "edit") {
-			this.editMode = true;
-			this.openEditForm(event.detail.row);
-		}
-	}
-    // Changed for pull request in vscode 2 3 4 5
-	handleInputChange(event) {
-		this.tempRow[event.target.name] = event.target.value;
-	}
-
-	deleteSelectedRow(deleteRow) {
-		let newData = JSON.parse(JSON.stringify(this._data));
-		newData = newData.filter((row) => row.uid !== deleteRow.uid);
-
-		// recalculate uids
-		newData.forEach((element, index) => (element.uid = index + 1));
-		this._data = newData;
-	}
-
-	handleCancel() {
-		this.tempRow = {};
-		this.showModal = false;
-		this.editMode = false;
-		this.editIndex = undefined;
-	}
-
-	handleAddRow() {
-		this.tempRow = {};
-		// refresh inputColumn
-		let tempInput = [...this._inputColumns];
-		tempInput.forEach((element) => (element.displayValue = undefined));
-		this._inputColumns = tempInput;
-		this.showModal = true;
-	}
-
-	handleSave() {
-		const allValid = [...this.template.querySelectorAll(`lightning-input`)].reduce((validSoFar, inputCmp) => {
-			inputCmp.reportValidity();
-			return validSoFar && inputCmp.checkValidity();
-		}, true);
-
-		if (!allValid) {
-			return;
-		}
-
-		let newData = JSON.parse(JSON.stringify(this._data));
-		if (!this.editMode) {
-			// you can use any unique and required field instead of 'uid'.
-			//calculate ui
-			this.tempRow.uid = this._data.length + 1;
-			newData.push(this.tempRow);
-		} else {
-			newData[this.editIndex] = this.tempRow;
-			this.editIndex = undefined;
-			this.editMode = false;
-		}
-		this._data = newData;
-		this.tempRow = {};
-		this.showModal = false;
-	}
-
-	openEditForm(editRow) {
-		this.editIndex = this._data.findIndex((row) => row.uid === editRow.uid);
-		this.tempRow = {...this._data[this.editIndex]};
-		this._inputColumns.forEach((element) => (element.displayValue = this.tempRow[element.fieldName]));
-		this.showModal = true;
-	}
 }
